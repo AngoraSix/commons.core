5cfa017231c2f8dc4416360f683cfd755fa9a064
Ger Roza <rozagerardo@gmail.com>
2023-07-30 23:56:28 -0300

    [Trello-xaq0NXuf] Added isAdminHint to SimpleContributor

--- src/main/kotlin/com/angorasix/commons/domain/SimpleContributor.kt
+++ src/main/kotlin/com/angorasix/commons/domain/SimpleContributor.kt
@@ -1,12 +1,22 @@
 package com.angorasix.commons.domain
 
+import org.springframework.data.annotation.PersistenceCreator
+import org.springframework.data.annotation.Transient
+
 /**
  * <p>
  * </p>
  *
  * @author rozagerardo
  */
-data class SimpleContributor constructor(
-    val id: String,
+data class SimpleContributor(
+    val contributorId: String,
     val grants: Set<String> = emptySet(),
-)
+    @Transient val isAdminHint: Boolean? = false,
+) {
+    @PersistenceCreator
+    constructor(
+        contributorId: String,
+        grants: Set<String> = emptySet(),
+    ) : this(contributorId, grants, null)
+}
--- src/main/kotlin/com/angorasix/commons/infrastructure/constants/AngoraSixInfrastructure.kt
+++ src/main/kotlin/com/angorasix/commons/infrastructure/constants/AngoraSixInfrastructure.kt
@@ -9,5 +9,8 @@ package com.angorasix.commons.infrastructure.constants
 class AngoraSixInfrastructure private constructor() {
     companion object {
         const val REQUEST_ATTRIBUTE_CONTRIBUTOR_KEY = "ANGORASIX_REQUEST_CONTRIBUTOR"
+
+        // This header shouldbe used only to resolve HATEOAS actions, it shouldn't be used for security decisions
+        const val REQUEST_IS_ADMIN_HINT_HEADER = "A6-IsAdminHint"
     }
 }
