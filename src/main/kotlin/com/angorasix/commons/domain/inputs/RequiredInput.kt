97d6d3bcb570a5bf6b2babcd719095be8989d590
Ger Roza <rozagerardo@gmail.com>
2024-11-09 19:44:05 -0300

    Added suppoer for HAL-JSON options for inline/body fields (dynamic options with Spring HATEOAS is not yet supported: #1695

---
+++ src/main/kotlin/com/angorasix/commons/domain/inputs/RequiredInput.kt
@@ -0,0 +1,36 @@
+package com.angorasix.commons.domain.inputs
+
+/**
+ * <p>
+ *     Similar to the HAL-FORM output, but to be transmitted inside the response body,
+ *     at least until Spring provides support for defining options dynamically:
+ *     https://docs.spring.io/spring-hateoas/docs/current/reference/html/#mediatypes.hal-forms.options.
+ * </p>
+ *
+ * @author rozagerardo
+ */
+data class InlineFieldSpec(
+    val name: String, // key for the UI to show the label appropriately
+    val type: FieldSpec,
+    val options: InlineFieldOptions? = null,
+) {
+    constructor(name: String, type: FieldSpec, options: List<OptionSpec>) : this(
+        name,
+        type,
+        InlineFieldOptions(emptyList(), options),
+    )
+}
+
+data class InlineFieldOptions(
+    val selectedValues: List<String>,
+    val inline: List<OptionSpec>,
+)
+
+data class OptionSpec(
+    val value: String,
+    val prompt: String, // key for the UI to show the label appropriately
+)
+
+enum class FieldSpec(val value: String) {
+    TEXT("text"), SELECT("select"), DATE("date"), TIME("time"), DATETIME("datetime")
+}
